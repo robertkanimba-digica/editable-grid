508bc403c75a590bc2ad66390098ed60c23ab9a9
robertkanimba-digica <102794754+robertkanimba-digica@users.noreply.github.com>
2022-09-01 11:32:52 +0200

    initial commit

---
+++ src/components/GridComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <li>{{ myGrid.text }}</li>
+  </form>
+  <button @click="count++">you pressed me {{ count }} times</button>
+</template>
+
+  <script setup>
+import { ref } from "vue";
+const props = defineProps({
+  myGrid: Object,
+});
+const count = ref(0);
+
+function addNewItem() {
+  var myObject = {
+    fileName: this,
+    firstName: "mike",
+    Surname: "peterson",
+    propertyDate: "10-22-1967",
+    propertySelect: "B",
+    propertyDropdown: 2,
+  };
+}
+</script>
